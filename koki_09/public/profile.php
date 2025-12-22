7ada2b3bfc86bdb799458b7bc5f3f013fb8394df
KITAGAWA Nao <muto@muto-ct.jp>
2025-12-22 09:00:02 +0900

    プロフィールにフォロー状況表示し、フォローできるように

--- koki_09/public/profile.php
+++ koki_09/public/profile.php
@@ -27,6 +27,22 @@ $select_sth = $dbh->prepare(
 $select_sth->execute([
   ':user_id' => $user_id,
 ]);
+
+// フォロー状態を取得
+$relationship = null;
+session_start();
+if (!empty($_SESSION['login_user_id'])) { // ログインしている場合
+  // フォロー状態をDBから取得
+  $select_sth = $dbh->prepare(
+    "SELECT * FROM user_relationships"
+    . " WHERE follower_user_id = :follower_user_id AND followee_user_id = :followee_user_id"
+  );
+  $select_sth->execute([
+    ':followee_user_id' => $user['id'], // フォローされる側は閲覧しようとしているプロフィールの会員
+    ':follower_user_id' => $_SESSION['login_user_id'], // フォローする側はログインしている会員
+  ]);
+  $relationship = $select_sth->fetch();
+}
 ?>
 <a href="/bbs.php">掲示板に戻る</a>
 
@@ -55,6 +71,16 @@ $select_sth->execute([
     </div>
   </div>
 </div>
+
+<?php if(empty($relationship)): // フォローしていない場合 ?>
+<div>
+  <a href="./follow.php?followee_user_id=<?= $user['id'] ?>">フォローする</a>
+</div>
+<?php else: // フォローしている場合 ?>
+<div>
+  <?= $relationship['created_at'] ?> にフォローしました。
+</div>
+<?php endif; ?>
 
 <div>
 <?php if(!empty($user['birthday'])): ?>
